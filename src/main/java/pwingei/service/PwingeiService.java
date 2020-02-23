14c5989d3655a73fa23ff1394cfc248151ba29b7
dgrummon <grummon1@hotmail.com>
2020-02-23 17:27:47 -0500

    added h2

--- src/main/java/pwingei/service/PwingeiService.java
+++ src/main/java/pwingei/service/PwingeiService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.extern.slf4j.Slf4j;
 import pwingei.entity.PwingeiEntity;
@@ -12,7 +11,6 @@ import pwingei.repository.PwingeiRepository;
 
 @Service
 @Slf4j
-@Transactional
 public class PwingeiService {
 
 	@Autowired
