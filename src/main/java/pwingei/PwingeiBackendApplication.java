63a3bc98047dac9cf6bc8063df004400ee689b3c
dgrummon <grummon1@hotmail.com>
2020-02-23 12:28:47 -0500

    First commit

---
+++ src/main/java/pwingei/PwingeiBackendApplication.java
@@ -0,0 +1,13 @@
+package pwingei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwingeiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwingeiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pwingei/PwingeiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pwingei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwingeiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
