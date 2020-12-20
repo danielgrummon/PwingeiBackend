7d1656006d70f536d597056300b59fba7679311a
Daniel Grummon <grummon1@hotmail.com>
2020-12-19 21:16:51 -0500

    coors issue

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,4 +15,4 @@ CREATE TABLE PWINGEI (
 INSERT INTO PWINGEI (ID, NAME, TYPE, LOCATION, DATE, IMAGEURL, IMAGEURL2, PRICE, DESCRIPTION) VALUES
   (1, 'Half Red', '40', 'Campoma Bridge', '2008', '/assets/images/campoma17.jpg', '/assets/images/campoma17b.jpg', '40', 'Number 17 is great because it has red and some not red.'),
   (2, 'Yellow Tail', '7', 'Campoma Bridge', '2008', '/assets/images/campoma7.jpg', '/assets/images/campoma7b.jpg', '40', 'Yellow tail is the best for people who really like to see yellow tails.'),
-  (3, 'Yellow Tail', '8', 'Campoma Bridge', '2008', '/assets/images/campoma8.jpg', '/assets/images/campoma8b.jpg', '50', 'Cant have enough top yellow swords.')
+  (3, 'Orange TYS', '8', 'Campoma Bridge', '2008', '/assets/images/campoma8.jpg', '/assets/images/campoma8b.jpg', '40', 'Cant have enough top yellow swords.')
