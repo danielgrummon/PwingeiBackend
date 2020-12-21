e14b4c82e45e1d1776674052ad9335e7b75b1d4f
Daniel Grummon <grummon1@hotmail.com>
2020-12-20 21:54:18 -0500

    updated price list

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,12 +16,12 @@ CREATE TABLE PWINGEI (
 
 INSERT INTO PWINGEI (ID, NAME, TYPE, LOCATION, DATE, IMAGEURL, IMAGEURL1, IMAGEURL2, PRICE, AVAILABLE, DESCRIPTION) VALUES
   (1, 'Yellow Tail',   '7',  'Campoma Bridge', '2008', '/assets/images/campoma7s.jpg',  '/assets/images/campoma7.jpg',  '/assets/images/campoma7b.jpg',  '35', 'NO',  'Add description here.'),
-  (2, 'Orange TYS',    '8',  'Campoma Bridge', '2008', '/assets/images/campoma8s.jpg',  '/assets/images/campoma8.jpg',  '/assets/images/campoma8b.jpg',  '35', 'NO',  'Add description here.'),  
+  (2, 'Orange TYS',    '8',  'Campoma Bridge', '2008', '/assets/images/campoma8s.jpg',  '/assets/images/campoma8.jpg',  '/assets/images/campoma8b.jpg',  '40', 'NO',  'Add description here.'),  
   (3, 'Yellow Dorsal', '9',  'Campoma Bridge', '2008', '/assets/images/campoma9s.jpg',  '/assets/images/campoma9.jpg',  '/assets/images/campoma9b.jpg',  '35', 'NO',  'Add description here.'),
   (4, 'Half Red',      '17', 'Campoma Bridge', '2008', '/assets/images/campoma17s.jpg', '/assets/images/campoma17.jpg', '/assets/images/campoma17b.jpg', '35', 'NO',  'Add description here.'),
-  (5, 'Saddleback',    '22', 'Campoma Bridge', '2008', '/assets/images/campoma22s.jpg', '/assets/images/campoma22.jpg', '/assets/images/campoma22b.jpg', '35', 'NO',  'Add description here.'),
-  (6, 'Red',           '28', 'Campoma Bridge', '2011', '/assets/images/campoma28s.jpg', '/assets/images/campoma28.jpg', '/assets/images/campoma28b.jpg', '35', 'NO',  'Add description here.'),
-  (7, 'Blue Snake',    '31', 'Campoma Bridge', '2011', '/assets/images/campoma31s.jpg', '/assets/images/campoma31.jpg', '/assets/images/campoma31b.jpg', '35', 'YES', 'Add description here.'),
+  (5, 'Saddleback',    '22', 'Campoma Bridge', '2008', '/assets/images/campoma22s.jpg', '/assets/images/campoma22.jpg', '/assets/images/campoma22b.jpg', '45', 'NO',  'Add description here.'),
+  (6, 'Red',           '28', 'Campoma Bridge', '2011', '/assets/images/campoma28s.jpg', '/assets/images/campoma28.jpg', '/assets/images/campoma28b.jpg', '40', 'NO',  'Add description here.'),
+  (7, 'Blue Snake',    '31', 'Campoma Bridge', '2011', '/assets/images/campoma31s.jpg', '/assets/images/campoma31.jpg', '/assets/images/campoma31b.jpg', '40', 'YES', 'Add description here.'),
   (8, 'Steel Tail',    '40', 'Campoma Bridge', '2011', '/assets/images/campoma40s.jpg', '/assets/images/campoma40.jpg', '/assets/images/campoma40b.jpg', '45', 'NO',  'Add description here.'),
   (9, 'Lavender',      '43', 'Campoma Bridge', '2011', '/assets/images/campoma43s.jpg', '/assets/images/campoma43.jpg', '/assets/images/campoma43b.jpg', '45', 'NO',  'Add description here.'),
   (10,'Aquamarine',    '45', 'Campoma Bridge', '2011', '/assets/images/campoma45s.jpg', '/assets/images/campoma45.jpg', '/assets/images/campoma45b.jpg', '45', 'NO',  'Add description here.'),
