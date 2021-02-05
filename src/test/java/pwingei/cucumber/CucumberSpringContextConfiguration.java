b1880f75bd62e3b4e3131eefbd5097917ddde6f9
danielgrummon <grummon1@hotmail.com>
2021-02-05 07:01:12 -0500

    added bdd

---
+++ src/test/java/pwingei/cucumber/CucumberSpringContextConfiguration.java
@@ -0,0 +1,16 @@
+package pwingei.cucumber;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CucumberSpringContextConfiguration {
+
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+}
